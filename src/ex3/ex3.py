0ec1788422b41927f7ca896b30ca7fdf7d1526d3
hslira <101367095+hslira@users.noreply.github.com>
2024-10-27 17:26:41 -0300

    setter e deleter

--- src/ex3/ex3.py
+++ src/ex3/ex3.py
@@ -22,6 +22,23 @@ class computed_property:
         else:
             return self.cached_value
 
+    def setter(self, func):
+        self.setter_func = func
+
+        return self
+
+    def __set__(self, instance, value):
+        self.setter_func(instance, value)
+        self.computed_once = False
+
+    def deleter(self, func):
+        self.deleter_func = func
+
+        return self
+
+    def __delete__(self, instance):
+        self.deleter_func(instance)
+
     def _compute(self, instance):
         print(f"computing {self.func.__name__}")
         calculated_attrib = self.func(instance)
@@ -29,15 +46,6 @@ class computed_property:
         self._set_cahced_attribs(instance)
 
         return self.cached_value
-
-    def _initialize_existing_params(self, instance):
-        for param in self.params:
-            try:
-                attrib = getattr(instance, param)
-            except AttributeError:
-                continue
-            self.params[param] = attrib
-            
 
     def _check_params_changed(self, instance):
         for param, value in self.params.items():
@@ -57,18 +65,30 @@ class computed_property:
                 continue
 
 
+class Circle:
+    def __init__(self, radius=1):
+        self.radius = radius
+
+    @computed_property("radius", "area")
+    def diameter(self):
+        """Circle diameter from radius"""
+        return self.radius * 2
+
+
+from math import sqrt
+
+
+class Vector:
+    def __init__(self, x, y, z, color=None):
+        self.x, self.y, self.z = x, y, z
+        self.color = color
+
+    @computed_property("x", "y", "z")
+    def magnitude(self):
+        return sqrt(self.x**2 + self.y**2 + self.z**2)
+
+
 def vector():
-    from math import sqrt
-
-    class Vector:
-        def __init__(self, x, y, z, color=None):
-            self.x, self.y, self.z = x, y, z
-            self.color = color
-
-        @computed_property("x", "y", "z")
-        def magnitude(self):
-            return sqrt(self.x**2 + self.y**2 + self.z**2)
-
     v = Vector(9, 2, 6)
     print(v.magnitude)
 
@@ -80,20 +100,15 @@ def vector():
 
 
 def circle():
-    class Circle:
-        def __init__(self, radius=1):
-            self.radius = radius
-
-        @computed_property("radius", "area")
-        def diameter(self):
-            return self.radius * 2
-
     circle = Circle()
     print(circle.diameter)
     print(circle.diameter)
     print(circle.diameter)
 
+    circle.radius = 10
+    print(circle.diameter)
+
 
 if __name__ == "__main__":
-    # vector()
+    vector()
     circle()
