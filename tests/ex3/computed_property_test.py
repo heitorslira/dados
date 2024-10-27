4517ce12e23f4f108588a558445bccf4379a490e
hslira <101367095+hslira@users.noreply.github.com>
2024-10-27 19:04:16 -0300

    tirando imports

--- tests/ex3/computed_property_test.py
+++ tests/ex3/computed_property_test.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
-from unittest.mock import patch
 from math import sqrt
 from src.ex3.ex3 import computed_property
+
 
 class TestComputedProperty(TestCase):
     def setUp(self) -> None:
@@ -13,12 +13,7 @@ class TestComputedProperty(TestCase):
             @computed_property("x", "y", "z")
             def magnitude(self):
                 return sqrt(self.x**2 + self.y**2 + self.z**2)
-            
-            
 
-            
-                
-        
         self.vector_class = Vector
 
         return super().setUp()
@@ -42,47 +37,45 @@ class TestComputedProperty(TestCase):
         magnitude2 = vector.magnitude
 
         self.assertEqual(magnitude1, magnitude2)
-    
+
     def test_accepts_non_existing_params(self):
         class Vector:
             def __init__(self, x, y, z, color=None):
                 self.x, self.y, self.z = x, y, z
                 self.color = color
 
-            @computed_property("x", "y", "z",'non_existing')
+            @computed_property("x", "y", "z", "non_existing")
             def magnitude(self):
                 return sqrt(self.x**2 + self.y**2 + self.z**2)
-            
-        
-        v = Vector(1,1,1)
+
+        v = Vector(1, 1, 1)
         magnitude = v.magnitude
-        
+
         self.assertTrue(True)
-        
+
     def test_accepts_mote_than_one_computed_prop(self):
         class Vector:
             def __init__(self, x, y, z, color=None):
                 self.x, self.y, self.z = x, y, z
                 self.color = color
 
-            @computed_property("x", "y", "z",'non_existing')
+            @computed_property("x", "y", "z", "non_existing")
             def magnitude(self):
                 return sqrt(self.x**2 + self.y**2 + self.z**2)
-            
+
             @computed_property("x", "y", "z")
             def soma(self):
                 return self.x + self.y + self.z
-            
-        
-        v = Vector(1,1,1)
+
+        v = Vector(1, 1, 1)
         magnitude = v.magnitude
         soma = v.soma
-        
+
         print(magnitude)
         print(soma)
-        
+
         self.assertTrue(True)
-        
+
     def test_computed_property_setter(self):
         class Circle:
             def __init__(self, radius=1):
@@ -90,28 +83,27 @@ class TestComputedProperty(TestCase):
 
             @computed_property("radius", "area")
             def diameter(self):
-                return self.radius * 2 
-            
+                return self.radius * 2
+
             @diameter.setter
             def diameter(self, diameter):
                 self.radius = diameter / 2
-                
+
             @diameter.deleter
             def diameter(self):
                 self.radius = 0
-        
+
         first_rad = 1
         circle = Circle(first_rad)
         diameter = circle.diameter
-        self.assertEqual(diameter, first_rad*2)
-        
+        self.assertEqual(diameter, first_rad * 2)
+
         second_radius = 5
-        circle.diameter = second_radius*2
-        
+        circle.diameter = second_radius * 2
+
         self.assertEqual(circle.radius, second_radius)
-        self.assertEqual(circle.diameter, second_radius*2)
-        
+        self.assertEqual(circle.diameter, second_radius * 2)
+
         del circle.diameter
-        
+
         self.assertEqual(circle.radius, 0)
-        
